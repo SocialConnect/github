647e307dee3dd9a7cb61961cb576923a56de0762
Dmitry Patsura <talk@dmtry.me>
2015-08-23 19:08:01 +0900

    Fix User entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,7 +4,7 @@
  * @author: Patsura Dmitry https://github.com/ovr <talk@dmtry.me>
  */
 
-namespace SocialConnect\Instagram\Entity;
+namespace SocialConnect\GitHub\Entity;
 
 class User extends \stdClass
 {
@@ -16,25 +16,5 @@ class User extends \stdClass
     /**
      * @var string
      */
-    public $bio;
-
-    /**
-     * @var string
-     */
-    public $website;
-
-    /**
-     * @var string
-     */
-    public $profile_picture;
-
-    /**
-     * @var string
-     */
-    public $full_name;
-
-    /**
-     * @var \stdClass
-     */
-    public $counts;
+    public $login;
 }
