aafd63e93f2d6bed0e16de5058b5252a288adf42
Dmitry Patsura <talk@dmtry.me>
2015-08-23 19:29:48 +0900

    Write Entity\User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -17,4 +17,59 @@ class User extends \stdClass
      * @var string
      */
     public $login;
+
+    /**
+     * @var string
+     */
+    public $avatar_url;
+
+    /**
+     * @var string
+     */
+    public $gravatar_id;
+
+    /**
+     * @var string
+     */
+    public $url;
+
+    /**
+     * @var string
+     */
+    public $html_url;
+
+    /**
+     * @var string
+     */
+    public $type;
+
+    /**
+     * @var string
+     */
+    public $public_repos;
+
+    /**
+     * @var string
+     */
+    public $public_gists;
+
+    /**
+     * @var string
+     */
+    public $followers;
+
+    /**
+     * @var string
+     */
+    public $following;
+
+    /**
+     * @var string
+     */
+    public $created_at;
+
+    /**
+     * @var string
+     */
+    public $updated_at;
 }
